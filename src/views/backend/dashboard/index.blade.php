e4699ed3096d075ba19288b8554207758eea735e
Karel Brijs <karel@chuck.be>
2021-01-11 01:35:52 +0000

    update dashboard overview layout

--- src/views/backend/dashboard/index.blade.php
+++ src/views/backend/dashboard/index.blade.php
@@ -1,112 +1,40 @@
-@extends('chuckcms::backend.layouts.admin')
+@extends('chuckcms::backend.layouts.base')
 
 @section('title')
 	E-commerce
 @endsection
 
-@section('breadcrumbs')
-	<ol class="breadcrumb">
-		<li class="breadcrumb-item active"><a href="{{ route('dashboard.module.ecommerce.index') }}">Overzicht</a></li>
-	</ol>
-@endsection
-
 @section('content')
-
-<!-- START ROW -->
-<div class="row">
-  <div class="col-lg-6 col-sm-12  d-flex flex-column">
-    
-    <!-- START WIDGET widget_weekly_sales_card-->
-    <div class="card no-border widget-loader-bar m-b-10">
-      <div class="container-xs-height full-height">
-        <div class="row-xs-height">
-          <div class="col-xs-height col-top">
-            <div class="card-header  top-left top-right">
-              <div class="card-title">
-                <span class="font-montserrat fs-11 all-caps">
-                	Opbrengst Laatste 7 Dagen <i class="fa fa-chevron-right"></i>
-                </span>
-              </div>
-            </div>
-          </div>
+<div class="container p-3 min-height">
+  <div class="row pb-3">
+    <div class="col-sm-6">
+      <div class="jumbotron jumbotron-fluid">
+        <div class="container pl-5">
+          <p class="lead">Revenue Past 7 Days</p>
+          <h1 class="display-4">{{ ChuckEcommerce::totalSalesLast7Days() }}</h1>
+          <p>{{ ChuckEcommerce::totalSalesLast7DaysQty() }} orders</p>
         </div>
-        <div class="row-xs-height">
-          <div class="col-xs-height col-top">
-            <div class="p-l-20 p-t-50 p-b-40 p-r-20">
-              <h3 class="no-margin p-b-5">{{ ChuckEcommerce::totalSalesLast7Days() }}</h3>
-              <span class="small hint-text pull-left">{{ ChuckEcommerce::totalSalesLast7DaysQty() }} bestellingen</span>
-            </div>
-          </div>
-        </div>
-
       </div>
     </div>
-    <!-- END WIDGET -->
-
-    <!-- START WIDGET widget_weekly_sales_card-->
-    <div class="card no-border widget-loader-bar m-b-10">
-      <div class="container-xs-height full-height">
-        <div class="row-xs-height">
-          <div class="col-xs-height col-top">
-            <div class="card-header  top-left top-right">
-              <div class="card-title">
-                <span class="font-montserrat fs-11 all-caps">Bestellingen <i class="fa fa-chevron-right"></i>
-	                        </span>
-              </div>
-              <div class="card-controls">
-                <ul>
-                  <li><a href="#" class="portlet-refresh text-black" data-toggle="refresh"><i class="portlet-icon portlet-icon-refresh"></i></a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
+    <div class="col-sm-6">
+      <div class="jumbotron jumbotron-fluid">
+        <div class="container pl-5">
+          <p class="lead">Total Revenue</p>
+          <h1 class="display-4">{{ ChuckEcommerce::totalSales() }}</h1>
+          <p>{{ $orders_count }} orders</p>
         </div>
-        <div class="row-xs-height">
-          <div class="col-xs-height col-top">
-            <div class="p-l-20 p-t-50 p-b-40 p-r-20">
-              <h3 class="no-margin p-b-5">{{ $orders_count }}</h3>
-              <span class="small hint-text pull-left">Totaal</span>
-            </div>
-          </div>
-        </div>
-        
       </div>
     </div>
-    <!-- END WIDGET -->
   </div>
-
-  <div class="col-lg-6 m-b-10 d-flex">
-    <!-- START WIDGET widget_pendingComments.tpl-->
-    <div class="widget-11-2 card no-border card-condensed no-margin widget-loader-circle align-self-stretch d-flex flex-column">
-      <div class="card-header top-right">
-        <div class="card-controls">
-          <ul>
-            <li><a data-toggle="refresh" class="portlet-refresh text-black" href="#"><i
-							class="portlet-icon portlet-icon-refresh"></i></a>
-            </li>
-          </ul>
-        </div>
-      </div>
-      <div class="padding-25">
-        <div class="pull-left">
-          <h2 class="text-success no-margin">{{ ChuckSite::getSite('name') }}</h2>
-          <p class="no-margin">Laatste 30 bestellingen</p>
-        </div>
-        <div class="pull-right">
-			<p class="no-margin ">Totaalomzet</p>
-        	<div class="clearfix"></div>
-        	<h3 class=" semi-bold">
-        		{{ ChuckEcommerce::totalSales() }}
-			</h3>
-		</div>
-      </div>
-      <div class="auto-overflow widget-11-2-table">
+  <div class="row bg-light shadow-sm rounded py-3 mb-3 mx-1">
+    <div class="col-12">
+      <div class="table-responsive">
         <table class="table table-condensed table-hover">
           <tbody>
-          	@foreach($orders as $order)
+            @foreach($orders->sortByDesc('created_at') as $order)
             <tr>
-              <td class="font-montserrat all-caps fs-12 w-75">Bestelling #{{ $order->json['order_number'] }}</td>
+              <td class="font-montserrat all-caps fs-12 w-25">{{ date('Y-m-d', strtotime($order->created_at)) }}</td>
+              <td class="font-montserrat all-caps fs-12 w-50">#{{ $order->json['order_number'] }}</td>
               <td class="w-25 b-l b-dashed b-grey">
                 <span class="font-montserrat fs-18">{{ ChuckEcommerce::formatPrice($order->final) }}</span>
               </td>
@@ -115,18 +43,12 @@
           </tbody>
         </table>
       </div>
-      <div class="padding-25 mt-auto">
-        <p class="small no-margin">
-          <a href="{{ route('dashboard.module.ecommerce.orders.index') }}"><i class="fa fs-16 fa-arrow-circle-o-down text-success m-r-10"></i></a>
-          <span class="hint-text ">Bekijk alle bestellingen</span>
-        </p>
-      </div>
     </div>
-    <!-- END WIDGET -->
+    <div class="col-12">
+      <a href="{{ route('dashboard.module.ecommerce.orders.index') }}" class="btn btn-block btn-primary">View all orders</a>
+    </div>
   </div>
 </div>
-<!-- END ROW -->
-
 @endsection
 
 @section('scripts')
