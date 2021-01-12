24f9861191eb0f90ae83712e4169b3ec9dfaef15
Karel Brijs <karel@chuck.be>
2021-01-12 11:35:02 +0000

    update order-detail layout

--- src/views/backend/orders/detail.blade.php
+++ src/views/backend/orders/detail.blade.php
@@ -1,4 +1,4 @@
-@extends('chuckcms::backend.layouts.admin')
+@extends('chuckcms::backend.layouts.base')
 
 @section('title')
 	Orders
@@ -9,37 +9,44 @@
 @endsection
 
 @section('css')
-	<link href="https://cdn.chuck.be/assets/plugins/jquery-datatable/media/css/dataTables.bootstrap.min.css" rel="stylesheet" type="text/css" />
-    <link href="https://cdn.chuck.be/assets/plugins/jquery-datatable/extensions/FixedColumns/css/dataTables.fixedColumns.min.css" rel="stylesheet" type="text/css" />
-    <link href="https://cdn.chuck.be/assets/plugins/datatables-responsive/css/datatables.responsive.css" rel="stylesheet" type="text/css" media="screen" />
+	
 @endsection
 
 @section('scripts')
-	<script src="https://cdn.chuck.be/assets/plugins/jquery-datatable/media/js/jquery.dataTables.min.js" type="text/javascript"></script>
-    <script src="https://cdn.chuck.be/assets/plugins/jquery-datatable/extensions/TableTools/js/dataTables.tableTools.min.js" type="text/javascript"></script>
-    <script src="https://cdn.chuck.be/assets/plugins/jquery-datatable/media/js/dataTables.bootstrap.js" type="text/javascript"></script>
-    <script src="https://cdn.chuck.be/assets/plugins/jquery-datatable/extensions/Bootstrap/jquery-datatable-bootstrap.js" type="text/javascript"></script>
-    <script type="text/javascript" src="https://cdn.chuck.be/assets/plugins/datatables-responsive/js/datatables.responsive.js"></script>
-    <script type="text/javascript" src="https://cdn.chuck.be/assets/plugins/datatables-responsive/js/lodash.min.js"></script>
-    <script src="https://cdn.chuck.be/assets/js/tables.js" type="text/javascript"></script>
-    <script src="https://cdn.chuck.be/assets/plugins/sweetalert2.all.js"></script>
-    
+	
 @endsection
 
 @section('content')
-<div class=" container-fluid container-fixed-lg">
+<div class="container">
     <div class="row">
-		<div class="col-lg-12">
-		<!-- START card -->
-			<div class="card card-transparent">
-				<div class="card-header ">
-					<div class="card-title">Orders</div>
-				</div>
-
-				<div class="card-block">
-					<h4>Gegevens 
-						<a href="#" type="button" data-target="#updateStatusModal" data-toggle="modal" class="label {{ ChuckEcommerce::getSetting('order.statuses.'.$order->status.'.paid') ? 'label-inverse' : '' }}">{{ ChuckEcommerce::getSetting('order.statuses.'.$order->status.'.short.'.app()->getLocale()) }}</a>
-					</h4>
+        <div class="col-sm-12">
+            <nav aria-label="breadcrumb">
+                <ol class="breadcrumb mt-3">
+					<li class="breadcrumb-item"><a href="{{ route('dashboard.module.ecommerce.orders.index') }}">Bestellingen</a></li>
+                    <li class="breadcrumb-item active" aria-current="page">Bestelling #{{ $order->json['order_number'] }}</li>
+                </ol>
+            </nav>
+        </div>
+    </div>
+	<div class="row">
+		<div class="col-sm-12">
+			<div class="jumbotron jumbotron-fluid">
+				<div class="container pl-5">
+					<p class="lead">Details</p>
+					
+					<div class="mb-3">
+						<a href="{{ route('dashboard.module.ecommerce.orders.delivery', ['order' => $order->id]) }}" class="btn btn-sm btn-outline-secondary rounded d-inline-block">
+							pakbon
+						</a>
+						@if($order->hasInvoice())
+						<a href="{{ route('dashboard.module.ecommerce.orders.invoice', ['order' => $order->id]) }}" class="btn btn-sm btn-outline-secondary rounded d-inline-block">
+							factuur
+						</a>
+						@endif
+					</div>
+					
+					<b>Status:</b>	<a href="#" type="button" data-target="#updateStatusModal" data-toggle="modal" class="badge badge-{{ ChuckEcommerce::getSetting('order.statuses.'.$order->status.'.paid') ? 'success' : 'info' }}">{{ ChuckEcommerce::getSetting('order.statuses.'.$order->status.'.short.'.app()->getLocale()) }}</a> <br><br>
+					
 					<b>Verzending:</b> {{ $order->json['shipping']['name'] }} <br>
                     <b>Verzendtijd:</b> {{ $order->json['shipping']['transit_time'] }} 
                     <br><br>
@@ -70,50 +77,50 @@
                     {{ $order->json['address']['shipping']['street'] . ' ' . $order->json['address']['shipping']['housenumber'] }}, <br> {{ $order->json['address']['shipping']['postalcode'] . ' ' . $order->json['address']['shipping']['city'] .', '. config('chuckcms-module-ecommerce.countries_data.'.$order->json['address']['shipping']['country'].'.native') }} <br>
                     @endif
 				</div>
-
-				<div class="card-block">
-					<div class="table-responsive">
-						<table class="table table-hover table-condensed" id="condensedTable" data-table-count="30">
-						<thead>
-							<tr>
-								<th style="width:8%">#</th>
-								<th style="width:28%">Product</th>
-								<th style="width:14%">Hvl</th>
-								<th style="width:34%">Prijs</th>
-								<th style="width:16%">Totaal</th>
-							</tr>
-						</thead>
-							<tbody>
-								@foreach($order->json['products'] as $sku => $product)
-								<tr class="order_line" data-id="{{ $sku }}">
-									<td class="v-align-middle semi-bold">{{ $loop->iteration }}</td>
-							    	<td class="v-align-middle semi-bold">{{ $product['title'] }}</td>
-							    	<td class="v-align-middle">{{ $product['quantity'] }}</td>
-							    	<td class="v-align-middle">{{ ChuckEcommerce::formatPrice($product['price_tax'])  }} </td>
-							    	<td class="v-align-middle semi-bold">{{ ChuckEcommerce::formatPrice($product['total'])  }}</td>
-							  	</tr>
-							  	@endforeach
-							  	<tr class="shipping_line">
-							  		<td class="v-align-middle semi-bold">{{ count($order->json['products']) + 1 }}</td>
-							    	<td class="v-align-middle semi-bold"></td>
-							    	<td class="v-align-middle"></td>
-							    	<td class="v-align-middle">Verzending </td>
-							    	<td class="v-align-middle semi-bold">{{ $order->shipping > 0 ? ChuckEcommerce::formatPrice($order->shipping + $order->shipping_tax) : 'gratis' }}</td>
-							  	</tr>
-							  	<tr class="total_line">
-							  		<td class="v-align-middle semi-bold">{{ count($order->json['products']) + 2 }}</td>
-							    	<td class="v-align-middle semi-bold"></td>
-							    	<td class="v-align-middle"></td>
-							    	<td class="v-align-middle">Totaal </td>
-							    	<td class="v-align-middle semi-bold">{{ ChuckEcommerce::formatPrice($order->final) }}</td>
-							  	</tr>
-							</tbody>
-						</table>
-					</div>
-				</div>
 			</div>
-		<!-- END card -->
 		</div>
+	</div>
+    <div class="row bg-light shadow-sm rounded py-3 mb-3 mx-1">
+        <div class="col-sm-12 my-3">
+        	<div class="table-responsive">
+        		<table class="table" style="width:100%">
+        			<thead>
+        				<tr>
+        					<th scope="col">#</th>
+							<th scope="col">Product</th>
+							<th scope="col">Hvl</th>
+							<th scope="col" class="pr-5">Prijs</th>
+							<th scope="col">Totaal</th>
+        				</tr>
+        			</thead>
+        			<tbody>
+						@foreach($order->json['products'] as $sku => $product)
+						<tr class="order_line" data-id="{{ $sku }}">
+							<td class="v-align-middle semi-bold">{{ $loop->iteration }}</td>
+							<td class="v-align-middle semi-bold">{{ $product['title'] }}</td>
+							<td class="v-align-middle">{{ $product['quantity'] }}</td>
+							<td class="v-align-middle">{{ ChuckEcommerce::formatPrice($product['price_tax'])  }} </td>
+							<td class="v-align-middle semi-bold">{{ ChuckEcommerce::formatPrice($product['total'])  }}</td>
+						</tr>
+						@endforeach
+						<tr class="shipping_line">
+							<td class="v-align-middle semi-bold">{{ count($order->json['products']) + 1 }}</td>
+							<td class="v-align-middle semi-bold"></td>
+							<td class="v-align-middle"></td>
+							<td class="v-align-middle">Verzending </td>
+							<td class="v-align-middle semi-bold">{{ $order->shipping > 0 ? ChuckEcommerce::formatPrice($order->shipping + $order->shipping_tax) : 'gratis' }}</td>
+						</tr>
+						<tr class="total_line">
+							<td class="v-align-middle semi-bold">{{ count($order->json['products']) + 2 }}</td>
+							<td class="v-align-middle semi-bold"></td>
+							<td class="v-align-middle"></td>
+							<td class="v-align-middle">Totaal </td>
+							<td class="v-align-middle semi-bold">{{ ChuckEcommerce::formatPrice($order->final) }}</td>
+						</tr>
+        			</tbody>
+        		</table>
+        	</div>
+        </div>
     </div>
 </div>
 
